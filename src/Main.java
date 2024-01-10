72d0ef1b63cbc793ed036007c319b9b478a7293e
YengSokroza <yengsokroza@gmail.com>
2024-01-10 15:47:04 +0700

    Exercise two

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square(10.0);
+        System.out.println("Square Area: " + square.getArea());
+        System.out.println("Square Perimeter: " + square.getPerimeter());
+    }
+}
